dd2f23799d77cf706041102d9db1b08da326c308
wekers <fernando@wekers.org>
2025-05-30 21:39:18 -0300

    Adjust error to reactive

--- src/main/java/com/devsuperior/workshopmongo/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/handlers/ControllerExceptionHandler.java
@@ -1,24 +1,22 @@
 package com.devsuperior.workshopmongo.controllers.handlers;
 
-import java.time.Instant;
-
+import com.devsuperior.workshopmongo.dto.CustomError;
+import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.devsuperior.workshopmongo.dto.CustomError;
-import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
-
-import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, ServerHttpRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
-        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getURI().toString());
         return ResponseEntity.status(status).body(err);
     }
 
