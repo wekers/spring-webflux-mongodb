1386dc3c1ad396a84ee909a6ecabb7e847326beb
wekers <fernando@wekers.org>
2025-06-02 16:28:20 -0300

    GET find posts fullsearch

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -10,6 +10,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.time.Instant;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -28,5 +30,17 @@ public class PostController {
 		public Flux<PostDTO> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
 			text = URL.decodeParam(text);
 			return service.searchByTitle(text);
+	}
+
+	@GetMapping(value = "/fullsearch")
+	public Flux<PostDTO> fullSearch(
+				@RequestParam(value = "text", defaultValue = "") String text,
+				@RequestParam(value = "minDate", defaultValue = "") String minDate,
+				@RequestParam(value = "maxDate", defaultValue = "") String maxDate) throws UnsupportedEncodingException, ParseException {
+
+			text = URL.decodeParam(text);
+			Instant min = URL.convertDate(minDate, Instant.EPOCH);
+			Instant max = URL.convertDate(maxDate, Instant.now());
+			return service.fullSearch(text, min, max);
 	}
 }
--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Instant;
+
 @Service
 public class PostService {
 
@@ -22,6 +24,13 @@ public class PostService {
 
 	public Flux<PostDTO> searchByTitle(String text) {
 		return repository.searchTitle(text)
+				.map(PostDTO::new)
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
+	}
+
+	public Flux<PostDTO> fullSearch(String text, Instant minDate, Instant maxDate) {
+		maxDate = maxDate.plusSeconds(86400);
+		return repository.fullSearch(text, minDate, maxDate)
 				.map(PostDTO::new)
 				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
 	}
