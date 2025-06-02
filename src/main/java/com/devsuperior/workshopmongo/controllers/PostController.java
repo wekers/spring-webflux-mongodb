26afd26542e18843aa879f341da164fe98d176a1
wekers <fernando@wekers.org>
2025-06-02 12:09:20 -0300

    GET post by id

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -1,21 +1,14 @@
 package com.devsuperior.workshopmongo.controllers;
 
-import java.io.UnsupportedEncodingException;
-import java.text.ParseException;
-import java.time.Instant;
-import java.util.List;
-
+import com.devsuperior.workshopmongo.dto.PostDTO;
+import com.devsuperior.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.devsuperior.workshopmongo.controllers.util.URL;
-import com.devsuperior.workshopmongo.dto.PostDTO;
-import com.devsuperior.workshopmongo.services.PostService;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -23,31 +16,10 @@ public class PostController {
 
 	@Autowired
 	private PostService service;
-/*
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<PostDTO> findById(@PathVariable String id) {
-		PostDTO dto = service.findById(id);
-		return ResponseEntity.ok(dto);
+		public Mono<ResponseEntity<PostDTO>> findById(@PathVariable String id) {
+			return service.findById(id)
+					.map(postDTO -> ResponseEntity.ok().body(postDTO));
 	}
-	
-	@GetMapping(value = "/titlesearch")
-	public ResponseEntity<List<PostDTO>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
-		text = URL.decodeParam(text);
-		List<PostDTO> list = service.findByTitle(text);
-		return ResponseEntity.ok(list);
-	}
-	
-	@GetMapping(value = "/fullsearch")
-	public ResponseEntity<List<PostDTO>> fullSearch(
-			@RequestParam(value = "text", defaultValue = "") String text,
-			@RequestParam(value = "minDate", defaultValue = "") String minDate,
-			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) throws UnsupportedEncodingException, ParseException {
-		
-		text = URL.decodeParam(text);
-		Instant min = URL.convertDate(minDate, Instant.EPOCH);
-		Instant max = URL.convertDate(maxDate, Instant.now());
-		
-		List<PostDTO> list = service.fullSearch(text, min, max);
-		return ResponseEntity.ok(list);
-	}*/
 }
--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,29 +1,22 @@
 package com.devsuperior.workshopmongo.services;
 
+import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
+import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class PostService {
 
 	@Autowired
 	private PostRepository repository;
-/*
-	@Transactional(readOnly = true)
-	public PostDTO findById(String id) {
-		Post post = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		return new PostDTO(post);
+
+	public Mono<PostDTO> findById(String id) {
+		return repository.findById(id)
+				.map(existingPost -> new PostDTO(existingPost))
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
 	}
-	
-	public List<PostDTO> findByTitle(String text) {
-		List<PostDTO> result = repository.searchTitle(text).stream().map(x -> new PostDTO(x)).toList();
-		return result;
-	}
-	
-	public List<PostDTO> fullSearch(String text, Instant minDate, Instant maxDate) {
-		maxDate = maxDate.plusSeconds(86400); // 24 * 60 * 60
-		List<PostDTO> result = repository.fullSearch(text, minDate, maxDate).stream().map(x -> new PostDTO(x)).toList();
-		return result;
-	}*/
+
 }
